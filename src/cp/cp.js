be5fa2732959216b302ba31825c53f49243b73da
Mila <99rest9900@gmail.com>
2022-12-05 14:37:41 +0300

    feat: add cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+
+export const spawnChildProcess = async (args) => {
+    const fPath = './files/script.js';
+    const cProcess = fork(fPath, args.splice(0, 2), { silent: true });
+    process.stdout.write('Enter your message\n');
+    process.stdin.pipe(cProcess.stdin);
+    cProcess.stdout.pipe(process.stdout);
+    cProcess.stdout.on('data', data => {
+        const msg = `Received from child process: ${data}`;
+        process.stdout.write(msg);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv);
