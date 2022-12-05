fe032f56a3e2078e068b44f8938df3d74dfdf2bb
Mila <99rest9900@gmail.com>
2022-12-05 14:32:19 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { promises as fsPromises } from 'fs';
+import { createHash } from 'node:crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fPath = './files/fileToCalculateHashFor.txt';
+    const content = await fsPromises.readFile(fPath)
+    console.log(createHash('sha256').update(content).digest('hex'));
 };
 
 await calculateHash();
