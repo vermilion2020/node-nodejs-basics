308395259954917b42651b6c22d67dff39f7b229
Mila <99rest9900@gmail.com>
2022-12-05 14:36:45 +0300

    feat: add wt solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
 const performCalculations = async () => {
-    // Write your code here
+    return new Promise((resolve, reject) => {
+        const results = [];
+        const systemCpuNumber = os.cpus().length;
+        for (let i = 0; i < systemCpuNumber; i++) {
+            const sourceNum = 10 + i;
+            
+            const worker = new Worker("./worker.js", { workerData: { sourceNum }});
+            worker.on("message", result => {
+            results.push({ status: 'resolved', data: result });
+            });
+            
+            worker.on("error", () => {
+                results.push({ status: 'error', data: null });
+            });
+
+            worker.on("exit", () => {
+                if (i === systemCpuNumber - 1) resolve(results);
+            });
+        }  
+    });
 };
 
-await performCalculations();
+
+console.log(await performCalculations());
